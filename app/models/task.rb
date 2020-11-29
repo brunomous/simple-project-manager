2dcb23b2ec2a19ce08d9b5c91ee5b65ddd4b7b3c
Jefferson Bruno <brnmouras@gmail.com>
2020-11-28 23:43:56 -0300

    refactor application structure

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['not_started', 'in_progress', 'complete'] }
+
+  STATUS_OPTIONS = [
+    ['Not started', 'not_started'],
+    ['In progress', 'in_progress'],
+    ['Complete', 'complete']
+  ]
 end
